96d0de915ac6149a84c72b17cf3ebcf8e0fafad2
itcast-wy <wangyong@itcast.cn>
2020-07-21 17:06:40 +0800

    3333333

--- src/main/java/Test.java
+++ src/main/java/Test.java
@@ -2,5 +2,7 @@ public class Test {
     public static void main(String[] args) {
         System.out.println("-----------------");
         System.out.println("-22222222222-----");
+
+        System.out.println("3333333333333");
     }
 }
